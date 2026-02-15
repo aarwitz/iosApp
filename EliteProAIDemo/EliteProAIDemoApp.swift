356129fcd9a842e3aa31ae629a6506d68bb99b2a
Aaron Horowitz <horwina@bu.edu>
2026-02-14 20:02:28 -0500

    Initial Commit

---
+++ EliteProAIDemo/EliteProAIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  EliteProAIDemoApp.swift
+//  EliteProAIDemo
+//
+//  Created by Aaron Horowitz on 2/14/26.
+//
+
+import SwiftUI
+
+@main
+struct EliteProAIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
